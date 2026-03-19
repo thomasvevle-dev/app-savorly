57dd0de9522b7681904f1ce15fd228ac060bb5a4
thomasvevle-dev <thomasvevle@gmail.com>
2026-03-19 22:19:15 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,14 @@
 const CACHE_NAME = "savorly-v1"
 
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll([
-        "/",
-        "/index.html",
-        "/manifest.json",
-        "/icon.png",
-        "/data/places.json"
-      ])
-    })
-  )
+  console.log("SW installed")
+  self.skipWaiting()
+})
+
+self.addEventListener("activate", event => {
+  console.log("SW activated")
 })
 
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request)
-    })
-  )
+  // enkel pass-through (ingen caching enda)
 })
